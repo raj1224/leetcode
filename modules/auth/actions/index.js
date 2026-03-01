8a177351fa448983542185c2152a09b75eb32225
Raj Kumar <imraaj1026@gmail.com>
2026-03-01 14:31:05 +0530

    navbar and role based authentication done

--- modules/auth/actions/index.js
+++ modules/auth/actions/index.js
@@ -41,3 +41,44 @@ export const onBoardUser= async()=>{
         }
     }
 }
+
+export const currentUserRole = async ()=>{
+  try {
+    const user = await currentUser();
+
+      if (!user) {
+            return { success: false, error: "No authenticated user found" };
+        }
+
+        const {id} = user;
+
+        const userRole = await db.user.findUnique({
+          where:{
+            clerkId:id
+          },
+          select:{
+            role:true
+          }
+        })
+    return userRole.role;
+  } catch (error) {
+     console.error("❌ Error fetching user role:", error);
+        return { success: false, error: "Failed to fetch user role" };
+  }
+}
+
+export const getCurrentUser = async()=>{
+  const user = await currentUser()
+
+  const dbUser = await db.user.findUnique({
+    where:{
+      clerkId:user.id
+    },
+    select:{
+      id:true
+    }
+  })
+
+
+  return dbUser;
+}
